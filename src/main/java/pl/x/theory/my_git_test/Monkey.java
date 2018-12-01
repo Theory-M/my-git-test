7faa31cad36dd5e060c0282f6e9d02d683119019
theory <mic.juszczak@gmail.com>
2018-12-01 14:34:59 +0100

    [add] - new class Monkey

---
+++ src/main/java/pl/x/theory/my_git_test/Monkey.java
@@ -0,0 +1,13 @@
+package pl.x.theory.my_git_test;
+
+public class Monkey extends Animal {
+
+    public Monkey(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
