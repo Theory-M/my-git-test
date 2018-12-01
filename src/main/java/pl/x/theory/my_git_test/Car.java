f75756908969c9b9a18bd83f8ec48e42d6f58c3f
theory <mic.juszczak@gmail.com>
2018-12-01 11:43:50 +0100

    [add] - rozszerzebue klasy Car

--- src/main/java/pl/x/theory/my_git_test/Car.java
+++ src/main/java/pl/x/theory/my_git_test/Car.java
@@ -4,4 +4,33 @@ public class Car {
 
     private String make;
     private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
