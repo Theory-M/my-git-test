7ff81791b003aaf3c3e36fa7bcbca1b13d85488c
theory <mic.juszczak@gmail.com>
2018-12-01 14:18:10 +0100

    [add] - new classes Dog and Cat extending Animal

---
+++ src/main/java/pl/x/theory/my_git_test/Cat.java
@@ -0,0 +1,12 @@
+package pl.x.theory.my_git_test;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
---
+++ src/main/java/pl/x/theory/my_git_test/Dog.java
@@ -0,0 +1,13 @@
+package pl.x.theory.my_git_test;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
