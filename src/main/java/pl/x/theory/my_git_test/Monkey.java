11ab4b29d038e955164b0e6c97c3b999bd646038
theory <mic.juszczak@gmail.com>
2018-12-01 15:04:09 +0100

    [change] - toString modification in Monkey

--- src/main/java/pl/x/theory/my_git_test/Monkey.java
+++ src/main/java/pl/x/theory/my_git_test/Monkey.java
@@ -14,7 +14,8 @@ public class Monkey extends Animal {
     @Override
     public String toString() {
         return "Monkey{" +
-                "name='" + name + '\'' +
+                "imie='" + name + '\'' +
                 '}';
     }
+
 }
